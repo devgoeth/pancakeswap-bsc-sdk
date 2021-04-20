f3a0eb5ec42d50ec800a625553ebca2b4a1c1921
Kondratiev Sergey <kondratiev@red-flag.ru>
2021-04-20 11:51:26 +0300

    getAddressFromFactory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./pancakeswap-bsc-sdk.cjs.production.min.js')
-} else {
-  module.exports = require('./pancakeswap-bsc-sdk.cjs.development.js')
-}
